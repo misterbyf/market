352d677a1383f62490dacca99cdf3f648472aaa4
bogdan <bogdaniys10@gmail.com>
2020-05-11 02:48:48 +0300

    add fetch and validation data

--- public/js/order.js
+++ public/js/order.js
@@ -1,4 +1,4 @@
-document.querySelector('#shop-order').onsubmit = (event) => {
+document.querySelector('#shop-order').onsubmit = async(event) => {
   event.preventDefault();
 
   let userName = document.querySelector('#username').value.trim();
@@ -7,10 +7,79 @@ document.querySelector('#shop-order').onsubmit = (event) => {
   let adress = document.querySelector('#adress').value.trim();
 
   if(!document.querySelector('#rule').checked){
-      //Нету подтверждения
+      swal({
+        icon: 'warning',
+        title: 'Warning',
+        text: 'Read and accept the rule',
+        button: {
+          text: 'OK'
+        }
+      })
+      return false;
   }
 
   if(userName == '' || phone == '' || email == '' || adress == ''){
-      //Пусто
+      swal({
+        icon: 'warning',
+        title: 'Warning',
+        text: 'Fill all fields',
+        button: {
+          text: 'OK'
+        }
+      })
+      return false;
+  }
+
+
+  let response = await request('/finish-order', 'POST', {
+    'username': userName,
+    'email': email,
+    'adress': adress,
+    'phone': phone,
+    'key': JSON.parse(localStorage.getItem('cart')),
+  });
+
+  if(response == 1) {
+    swal({
+      icon: 'success',
+      title: 'Message',
+      text: 'Message has been send',
+      button: {
+        text: 'OK'
+      }
+    });
+    return false;
+  } else {
+    swal({
+      icon: 'warning',
+      title: 'Error',
+      text: 'Problem with mail',
+      button: {
+        text: 'OK'
+      }
+    });
+    return false;
+  }  
+}
+
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    if (data) {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify(data);
+    }
+    updateToLocaleStorage();
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    });
+    return await response.json();
+  } catch (e) {
+    console.warn('ERROR', e.message);
   }
 }
